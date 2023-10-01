9e9d3411a5e534c66d1484561436ad24cf2caeb5
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2023-09-30 21:21:49 -0700

    Use BlockView API v2 to retrieve biomes
    - Require Fabric API >=0.88.1
    - Require exactly Minecraft 1.20.1

--- src/main/java/me/pepperbell/continuity/client/mixinterface/ChunkRendererRegionExtension.java
+++
@@ -1,9 +0,0 @@
-package me.pepperbell.continuity.client.mixinterface;
-
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.biome.Biome;
-
-public interface ChunkRendererRegionExtension {
-	RegistryEntry<Biome> continuity$getBiome(BlockPos pos);
-}
--- src/main/java/me/pepperbell/continuity/client/util/biome/BiomeRetriever.java
+++ src/main/java/me/pepperbell/continuity/client/util/biome/BiomeRetriever.java
@@ -1,16 +1,11 @@
 package me.pepperbell.continuity.client.util.biome;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.Nullable;
 
 import grondag.canvas.terrain.region.input.InputRegion;
-import me.jellysquid.mods.sodium.client.world.WorldSlice;
-import me.pepperbell.continuity.client.mixinterface.ChunkRendererRegionExtension;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
-import net.minecraft.world.WorldView;
 import net.minecraft.world.biome.Biome;
 
 public final class BiomeRetriever {
@@ -18,17 +13,6 @@ public final class BiomeRetriever {
 
 	private static Provider createProvider() {
 		ClassLoader classLoader = BiomeRetriever.class.getClassLoader();
-
-		if (FabricLoader.getInstance().isModLoaded("sodium")) {
-			try {
-				Class<?> worldSliceClass = Class.forName("me.jellysquid.mods.sodium.client.world.WorldSlice", false, classLoader);
-				worldSliceClass.getMethod("getBiomeAccess");
-				return BiomeRetriever::getBiomeByWorldSlice;
-			} catch (ClassNotFoundException | NoSuchMethodException e) {
-				//
-			}
-			return BiomeRetriever::getBiomeByWorldView;
-		}
 
 		if (FabricLoader.getInstance().isModLoaded("canvas")) {
 			try {
@@ -38,13 +22,9 @@ public final class BiomeRetriever {
 			} catch (ClassNotFoundException | NoSuchMethodException e) {
 				//
 			}
-			return BiomeRetriever::getBiomeByWorldView;
 		}
 
-		if (ArrayUtils.contains(ChunkRendererRegion.class.getInterfaces(), ChunkRendererRegionExtension.class)) {
-			return BiomeRetriever::getBiomeByExtension;
-		}
-		return BiomeRetriever::getBiomeByWorldView;
+		return BiomeRetriever::getBiomeByAPI;
 	}
 
 	@Nullable
@@ -56,28 +36,11 @@ public final class BiomeRetriever {
 	}
 
 	@Nullable
-	private static Biome getBiomeByWorldView(BlockRenderView blockView, BlockPos pos) {
-		if (blockView instanceof WorldView worldView) {
-			return worldView.getBiome(pos).value();
+	private static Biome getBiomeByAPI(BlockRenderView blockView, BlockPos pos) {
+		if (blockView.hasBiomes()) {
+			return blockView.getBiomeFabric(pos).value();
 		}
 		return null;
-	}
-
-	@Nullable
-	private static Biome getBiomeByExtension(BlockRenderView blockView, BlockPos pos) {
-		if (blockView instanceof ChunkRendererRegionExtension extension) {
-			return extension.continuity$getBiome(pos).value();
-		}
-		return getBiomeByWorldView(blockView, pos);
-	}
-
-	// Sodium
-	@Nullable
-	private static Biome getBiomeByWorldSlice(BlockRenderView blockView, BlockPos pos) {
-		if (blockView instanceof WorldSlice worldSlice) {
-			return worldSlice.getBiomeAccess().getBiome(pos).value();
-		}
-		return getBiomeByWorldView(blockView, pos);
 	}
 
 	// Canvas
@@ -86,7 +49,7 @@ public final class BiomeRetriever {
 		if (blockView instanceof InputRegion inputRegion) {
 			return inputRegion.getBiome(pos);
 		}
-		return getBiomeByWorldView(blockView, pos);
+		return getBiomeByAPI(blockView, pos);
 	}
 
 	private interface Provider {
